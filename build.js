4c4e9bc0a7bc82eb89026819e66644108f53905e
道化師 <wraith13@hotmail.co.jp>
2021-03-22 19:37:30 +0900

    🛠 実装 ( WIP )

--- build.js
+++ build.js
@@ -1,38 +1,58 @@
 'use strict';
 const fs = require("fs");
 const fget = path => fs.readFileSync(path, { encoding: "utf-8" });
-const images = require("./resource/images.json");
+const json = require("./build.json");
 fs.writeFileSync
 (
-    "./index.html",
-    fget("./index.template.html")
-        .replace
-        (
-            /__BUILD_MESSAGE__/g,
-            "This file is genereted by build.js. See README.md. このファイルは build.js によって生成されました。README.md を参照してください。"
-        )
-        .replace
-        (
-            /__STYLE__/g,
-            fget("./css/index.css")
-        )
-        .replace
-        (
-            /__EVIL_COMMONJS__/g,
-            fget("./script/evil-commonjs/index.js")
-        )
-        .replace
-        (
-            /__SCRIPT__/g,
-            fget("./script/index.js")
-        )
-        .replace
-        (
-            /__IMAGES__/g,
-            Object.keys(images)
-                .map(id => `<div id="${id}">${fget(images[id]).replace(/[\w\W]*(<svg)/g, "$1")}</div>`)
-                .join("")
-        )
-);
+    json.output.path,
+    Object.keys(json.parameters).map
+    (
+        key =>
+        {
+            const value = json.parameters[key];
+            let work = null;
+            if ("string" === typeof value)
+            {
+                work = value;
+            }
+            else
+            if ("string" === typeof value.path)
+            {
+                work = fget(value.path);
+            }
+            else
+            if ("string" === typeof value.resource)
+            {
+                const resource = require(value.resource);
+                work = Object.keys(resource)
+                    .map(id => `<div id="${id}">${fget(resource[id]).replace(/[\w\W]*(<svg)/g, "$1")}</div>`)
+                    .join("");
+            }
+            else
+            if (undefined !== value.call)
+            {
+                if ("timestamp" === value.call)
+                {
+                    work = `${new Date()}`;
+                }
+                else
+                {
+                    console.error(`unknown call: ${key}: ${JSON.stringify(value)}`);
+                }
+            }
+            else
+            {
+                console.error(`unknown parameter: ${key}: ${JSON.stringify(value)}`);
+            }
+            return { key, work };
+            //html = html.replace(new RegExp(key, "g"), value);
+        }
+    )
+    .reduce
+    (
+        (r, p) => "string" === typeof p.work ? r.replace(new RegExp(p.key, "g"), p.work): r,
+        fget(json.template.path)
+    )
+)
 
-// how to run: node ./build.js
+// how to run: `node ./build.js`
