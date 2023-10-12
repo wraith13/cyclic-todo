8814c7525136d4dbb94d68c4a282c936a975cc90
道化師 <wraith13@hotmail.co.jp>
2023-10-13 07:16:34 +0900

    🛠 実装 ( WIP )

--- build/index.ts
+++ build/index.ts
@@ -1,14 +1,74 @@
 import pomeJson from "../resource/poem.json";
 import { locale } from "../script/locale.js";
-
 const fs = require("fs");
+const escapeHtml = (text: string) => text
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+const makeAttributes = (source: any): string =>
+    Object.keys(source)
+    .map
+    (
+        (key: string) =>
+        {
+            const value = source[key];
+            switch(key)
+            {
+            case "tag":
+                return null;
+            case "children":
+                return null;
+            case "className":
+                return `class="${value}"`;
+            }
+            return `${key}="${encodeURI(value)}"`;
+        }
+    )
+    .filter(i => null !== i)
+    .join(" ");
+const makeHtml = (source: any): string =>
+{
+    if ("string" === typeof source)
+    {
+        return escapeHtml(source);
+    }
+    if (Array.isArray(source))
+    {
+        return source.map(i => makeHtml(i)).join("");
+    }
+    if ("object" === typeof source && "tag" in source)
+    {
+        if ("children" in source)
+        {
+            var result = "<";
+            result += (`${source.tag} ` +makeAttributes(source)).trim();
+            result += ">";
+            result += makeHtml(source.children);
+            result += `</${source.tag}>`;
+            return result;
+        }
+        else
+        {
+            var result = "<";
+            result += (`${source.tag} ` +makeAttributes(source)).trim();
+            result += "/>";
+            return result;
+        }
+    }
+    return `${source}`;
+};
+export const JsonHeader = "JSON:";
+const stringOrJson = (text: string): string =>
+    text.startsWith(JsonHeader) ? makeHtml(JSON.parse(text.substring(JsonHeader.length))): text;
 const makePoem = (key: keyof typeof pomeJson) =>
 {
     var result = "";
     result += "<div class=\"poem\">\r\n";
     result += `<span class=\"poem-title\">${locale.string(`poem.${key}.title`)}</span>\r\n`;
     result += `<span class=\"poem-subtitle\">${locale.string(`poem.${key}.subtitle`)}</span>\r\n`;
-    result += `<span class=\"poem-description\">${locale.string(`poem.${key}.description`)}</span>\r\n`;
+    result += `<span class=\"poem-description\">${stringOrJson(locale.string(`poem.${key}.description`))}</span>\r\n`;
     result += `<span class=\"poem-image\">${pomeJson[key]}</span>\r\n`;
     result += "</div>\r\n";
     return result;
